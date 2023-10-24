9038402608ea691ea4e6c78d9f2022ff0b8c7abf
MaximKalenkovich <peterson.maxim@mail.ru>
2023-10-24 10:24:31 +0300

    add a

--- src/components/first/FirstSsaa.tsx
+++ src/components/first/FirstSsaa.tsx
@@ -3,8 +3,8 @@ export const First = () =>{
     return(
         <div>
             <ul>
-                <li></li>
-                <li></li>
+                <li><a href='£'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel dicta quas natus. Dolore dolorem, voluptatum quisquam mollitia tempore excepturi numquam ut, quo repellendus exercitationem nam. Non nesciunt quis voluptate vero.</a></li>
+                
             </ul>
         </div>
     )
